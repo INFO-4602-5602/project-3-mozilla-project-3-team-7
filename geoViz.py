6741a5b60bd83668d8c63fe78a8d0d287d23e830
Joshua Griffiths <joshuagriffiths@rgntgw2-71-98-dhcp.int.colorado.edu>
2018-04-05 20:46:56 -0600

    Finished geo viz, needs interactivity still

--- geoViz.py
+++ geoViz.py
@@ -7,19 +7,22 @@
 #Heat map of average importance of privacy by country
 #Based on a 1-10 Scale
 
-from bokeh.io import show
-from bokeh.models import (
-    ColumnDataSource,
-    HoverTool,
-    LogColorMapper
-)
-from bokeh.palettes import Viridis6 as palette
-from bokeh.plotting import figure
+import geopandas as gpd
+import csv
+import matplotlib.pyplot as plt
+import pandas as pd
 
-from bokeh.sampledata.us_counties import data as counties
-from bokeh.sampledata.unemployment import data as unemployment
+#Gets all countries with lats and longs
+world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+#Parses just the names and the longs,lats
+countries = world[['name','geometry']]
 
-palette.reverse()
+#Appends the privacy rating for each country on to our dataset
+privacy = pd.read_csv('geoData.csv')
+countryPrivacy = countries.merge(privacy, on = 'name')
 
+#Plot the dataset on to a HeatMap based on privacy rating
+countryPrivacy.plot(column='privacy', cmap='OrRd')
 
-
+#Show the plot
+plt.show()
