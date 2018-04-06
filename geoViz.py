a1c5a961ad546b86d5c675f98e1580cb41d4b795
Joshua Griffiths <joshuagriffiths@rgntgw2-71-225-dhcp.int.colorado.edu>
2018-04-06 17:43:43 -0600

    Tried to make interactive, impossible with geoDataFrames, tried to add title, impossible with geoDataFrames, added legend

--- geoViz.py
+++ geoViz.py
@@ -12,17 +12,34 @@ import csv
 import matplotlib.pyplot as plt
 import pandas as pd
 
+from bokeh.io import show
+from bokeh.models import (
+    ColumnDataSource,
+    HoverTool,
+    LogColorMapper
+)
+from bokeh.palettes import Viridis6 as palette
+from bokeh.plotting import figure
+
+from shapely.geometry import Point
+palette.reverse()
+
 #Gets all countries with lats and longs
 world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
 #Parses just the names and the longs,lats
 countries = world[['name','geometry']]
+
 
 #Appends the privacy rating for each country on to our dataset
 privacy = pd.read_csv('geoData.csv')
 countryPrivacy = countries.merge(privacy, on = 'name')
 
 #Plot the dataset on to a HeatMap based on privacy rating
-countryPrivacy.plot(column='privacy', cmap='OrRd')
+countryPrivacy.plot(column='privacy', cmap='OrRd',legend = True)
+#gpd.title('Country vs Importance of Privacy')
+
+
+
 
 #Show the plot
 plt.show()
